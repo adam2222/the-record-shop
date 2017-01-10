8e2425d1b0e058d4157b998eb3efae6de5d7abc4
James Oh <jamesboh@gmail.com>
2017-01-10 15:59:54 -0500

    add credit card model associations

--- db/models/index.js
+++ db/models/index.js
@@ -7,9 +7,13 @@
 const User = require('./user')
 const Puppy = require('./puppy')
 const Shipping = require('./shipping')
+const CreditCard = require('./credit_card')
 
 Puppy.belongsTo(User)
 User.hasMany(Puppy)
 Shipping.belongsTo(User)
+User.hasMany(CreditCard)
+CreditCard.belongsTo(User)
 
-module.exports = {User, Puppy, Shipping}
+
+module.exports = {User, Puppy, Shipping, CreditCard}
