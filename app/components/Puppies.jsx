7bfaa84175b74e7777c8c19cb6fcfe50ea65aeb8
Adam M. Intrator <adam.intra2r@gmail.com>
2017-01-10 15:33:41 -0500

    more

---
+++ app/components/Puppies.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import HeaderContainer from '../containers/HeaderContainer'
+
+export default class Puppies extends Component {
+  render(){
+    return (
+      <div>
+        Puppies
+      </div>
+    )
+  }
+
+}
---
+++ app/components/Puppy.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import HeaderContainer from '../containers/HeaderContainer'
+
+export default class Puppy extends Component {
+  render(){
+    return (
+      <div>
+        Puppy
+      </div>
+    )
+  }
+
+}
