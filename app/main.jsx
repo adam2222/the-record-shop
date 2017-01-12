eb6d19ee32f0c1f6c2ccd194b85a5ff4cfe1ca58
David Godow <dmgodow@gmail.com>
2017-01-12 17:59:53 -0500

    Initial album container

--- app/main.jsx
+++ app/main.jsx
@@ -1,20 +1,19 @@
 'use strict'
 import React from 'react'
+import ReactDOM from 'react-dom'
 import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
 import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 import App from './components/App'
-import HomeContainer from './containers/HomeContainer'
+import AlbumContainer from './containers/AlbumContainer'
 
 import store from './store'
 
-render (
+ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <Route path="/albums" component={AlbumsContainer} />
         <Route path="/albums/:albumId" component={AlbumContainer} />
-        <IndexRedirect to="/home" />
       </Route>
     </Router>
   </Provider>,
