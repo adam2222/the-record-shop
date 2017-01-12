979756e35d1092585fd986c996f7800522161e8f
David Godow <dmgodow@gmail.com>
2017-01-12 12:57:05 -0500

    Home react component setup

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import HeaderContainer from '../containers/HeaderContainer'
+import HomeContainer from '../containers/HomeContainer'
+import Footer from './footer'
 
 export default class App extends Component {
   render(){
     return (
       <div>
-        <div className="col-xs-12">
-          <HeaderContainer />
-        </div>
-
-        <div className="col-xs-12">
-          {this.props.children}
-        </div>
+        <HeaderContainer />
+        <HomeContainer />
+        <Footer />
       </div>
     )
   }
-
 }
