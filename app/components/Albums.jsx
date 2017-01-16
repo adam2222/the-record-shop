0d613094a090f05955c461af65568e4a833f0c60
Adam M. Intrator <adam.intra2r@gmail.com>
2017-01-15 19:56:40 -0500

    User can login using local strategy

--- app/components/Albums.jsx
+++ app/components/Albums.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router'
 export default class AllAlbums extends Component {
 
   render() {
-    const arrayOfAlbums = this.props.filteredAlbums[0] ? this.props.filteredAlbums : this.props.allAlbums
+    const filteredAlbums = this.props.filteredAlbums
+    const allAlbums = this.props.allAlbums
+    const arrayOfAlbums = filteredAlbums[0] ? filteredAlbums : allAlbums
 
     const renderedAlbums = arrayOfAlbums.map(album => {
       return (
--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -3,45 +3,56 @@ import React from 'react'
 export const Login = (props) => {
   const authenticated = props.authenticated;
   const login = props.login;
+  const logout = props.logout;
   console.log('AUTHENTICATED', authenticated)
 
   const loggedOut = (
     <ul className="nav navbar-nav navbar-right" id="blue">
-      <li>
-        <form className="navbar-form" onSubmit={evt => {
-          evt.preventDefault()
-          login(evt.target.username.value, evt.target.password.value)
-        } }>
-          <input className="form-control" name="username" placeholder="Email"/>
-          <input className="form-control" name="password" type="password" placeholder="Password" />
-          <button className="btn btn-default" type="submit">Log In</button>
-        </form>
-      </li>
-      <a className="btn btn-social-icon btn-github">
-        <span className="fa fa-github"></span>
-      </a>
+      <li><a href="#"><span className="glyphicon glyphicon-shopping-cart"></span></a></li>
+      <li className="dropdown">
+         <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sign In <span className="caret"></span></a>
+         <ul className="dropdown-menu">
+           <li>
+             <form className="navbar-form" onSubmit={evt => {
+                 evt.preventDefault()
+                 login(evt.target.username.value, evt.target.password.value)
+               } }>
+               <input className="form-control" name="username" placeholder="Email"/>
+               <input className="form-control" name="password" type="password" placeholder="Password" />
+               <button className="btn btn-default" type="submit">Log In</button>
+             </form>
+           </li>
+           <li role="separator" className="divider"></li>
+           <li><a className="btn btn-social-icon btn-github">
+             <span className="fa fa-github"></span>
+           </a></li>
+         </ul>
+       </li>
       <li><a href="#">Sign Up</a></li>
     </ul>
   )
 
+
   const loggedIn = (
-    <ul className="nav navbar-nav navbar-right" id="blue">
-      <li><a href="#"><span className="glyphicon glyphicon-shopping-cart"></span></a></li>
-      <li><a href="#">Hello Username!</a></li>
-      <li><a href="#">Log Out!</a></li>
-    </ul>
+    <div>
+      <ul className="nav navbar-nav navbar-right" id="blue">
+        <li><a href="#"><span className="glyphicon glyphicon-shopping-cart"></span></a></li>
+        <li><a href="#">Hello {authenticated && authenticated.firstName}!</a></li>
+        <button className="btn btn-default navbar-btn" onClick={logout}>Log Out</button>
+      </ul>
+    </div>
   )
 
   return authenticated ? loggedIn : loggedOut
 
 }
 
-import {login} from 'APP/app/reducers/auth'
+import {login, logout} from 'APP/app/reducers/auth'
 import {connect} from 'react-redux'
 
 export default connect (
   state => ({
     authenticated: state.auth
   }),
-  {login},
+  {login, logout},
 ) (Login)
