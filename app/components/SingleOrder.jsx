54bbbae554698a0e5dfddcb78e78d49ea92860e3
Adam M. Intrator <adam.intra2r@gmail.com>
2017-01-18 09:53:48 -0500

    single order view working

--- app/components/SingleOrder.jsx
+++ app/components/SingleOrder.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const orderList = ({ orders }) => {
-  console.log(orders)
+const orderList = ({ order }) => {
+
+
+
   return (
     <div>
-      Hello World
+      <h2>Order Id {order.id}</h2>
+      <ul className="list-group">
+        <li className="list-group-item">Status: {order.status}</li>
+        <li className="list-group-item">Date Created: {order.date_created}</li>
+        <li className="list-group-item">Date Shipped: {order.date_shipped}</li>
+        <li className="list-group-item">Date Delivered: {order.date_delivered}</li>
+        <li className="list-group-item">Albums Purchased:
+          <ul className="list-group">
+              {order.items && order.items.map(item => {
+                  return (
+                    <li className="list-group-item">{item.title}: {item.quantity}</li>
+                  )
+                })
+              }
+          </ul></li>
+        <li className="list-group-item">Grand Total: ${order.total}</li>
+      </ul>
     </div>
   )
 }
