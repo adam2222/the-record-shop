f7c54d42cea41cc62463b615ce2aa795af3a6d67
James Oh <jamesboh@gmail.com>
2017-01-18 02:15:32 -0500

    copy update

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default () => {
 	return (
 		<div className="hero">
-			<h1>The Best Collection Ever...</h1>
+			<h1>The Best Collection Ever!!!</h1>
 			<img src="./images/record-store-photo.jpg" />
 		</div>
 	)
